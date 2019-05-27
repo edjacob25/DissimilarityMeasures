fd316b0863142ae83e94c99489ef5adfde7638ed
Jacob Rivera <edjacob25@gmail.com>
2019-05-27 09:19:47 -0500

    Updated modified measures

--- src/main/java/weka/core/EskinModified.kt
+++ src/main/java/weka/core/EskinModified.kt
@@ -1,5 +1,23 @@
 package weka.core
 
-class EskinModified : Eskin() {
+import me.jacobrr.LearningCompanion
 
+class EskinModified : Eskin() {
+    protected lateinit var learningCompanion: LearningCompanion
+
+    override fun setInstances(insts: Instances?) {
+        super.setInstances(insts)
+        learningCompanion = LearningCompanion("N", "K")
+        learningCompanion.trainClassifiers(instances)
+    }
+
+    override fun difference(index: Int, val1: String, val2: String): Double {
+        val baseDifference = super.difference(index, val1, val2)
+        val kappa = learningCompanion.weights[index]!!
+        if (kappa < 0.5) {
+            return 1.0
+        }
+        val normalized = kappa * baseDifference
+        return normalized
+    }
 }
--- src/main/java/weka/core/GambaryanModified.kt
+++ src/main/java/weka/core/GambaryanModified.kt
@@ -1,4 +1,23 @@
 package weka.core
 
+import me.jacobrr.LearningCompanion
+
 class GambaryanModified: Gambaryan() {
+    protected lateinit var learningCompanion: LearningCompanion
+
+    override fun setInstances(insts: Instances?) {
+        super.setInstances(insts)
+        learningCompanion = LearningCompanion("N", "K")
+        learningCompanion.trainClassifiers(instances)
+    }
+
+    override fun difference(index: Int, val1: String, val2: String): Double {
+        val baseDifference = super.difference(index, val1, val2)
+        val kappa = learningCompanion.weights[index]!!
+        if (kappa < 0.5) {
+            return 1.0
+        }
+        val normalized = kappa * baseDifference
+        return normalized
+    }
 }

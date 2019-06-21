faef3e477809ebfa1bccb7d834593c449765f360
Jacob Rivera <edjacob25@gmail.com>
2019-06-21 18:42:59 -0500

    Return measure times kappa in Original Measures

--- src/main/java/me/jacobrr/ModifiedCompanion.kt
+++ src/main/java/me/jacobrr/ModifiedCompanion.kt
@@ -11,9 +11,6 @@ class ModifiedCompanion(instances: Instances) {
 
     fun calculateDistance(baseDifference: Double, index: Int): Double {
         val kappa = learningCompanion.weights[index]!!
-        if (kappa < 0.5) {
-            return baseDifference
-        }
         val normalized = kappa * baseDifference
         return normalized
     }

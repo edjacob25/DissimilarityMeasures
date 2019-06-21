d360dbb700a9f47af501f06ef3fd96010d72edea
Jacob Rivera <edjacob25@gmail.com>
2019-06-21 18:20:05 -0500

    Updated kotlin version

--- src/main/java/weka/core/LinModified2.kt
+++
@@ -1,7 +0,0 @@
-package weka.core
-
-open class LinModified2 : LinModified() {
-    override fun difference(index: Int, val1: String, val2: String): Double {
-        return learningCompanion.weights[index]!! * super.difference(index, val1, val2)
-    }
-}

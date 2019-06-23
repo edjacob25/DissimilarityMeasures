8b0bd03f6a14ef42bda6560cd888163003f7bc5a
Jacob Rivera <edjacob25@gmail.com>
2019-06-23 15:00:53 -0500

    Changed measures to use (1 - kappa) sending original when kappa is low

--- src/main/java/weka/core/LearningBasedDissimilarity.kt
+++ src/main/java/weka/core/LearningBasedDissimilarity.kt
@@ -10,7 +10,7 @@ open class LearningBasedDissimilarity : BaseCategoricalDistance() {
     protected var strategy = "A"
     protected var weightStyle = "N"
     protected var symmetric = false
-    protected var saveSecond = Pair(true, "auc")
+    protected var saveSecond = Pair(true, "kappa")
 
     override fun setInstances(insts: Instances?) {
         super.setInstances(insts)
